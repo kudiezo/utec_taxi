5b0667493935eda1681aa9258600ff4f3890f0c2
AOneManShow <el.mateus727@gmail.com>
2023-07-03 20:43:23 +0100

    Sidebar renomeada e bem separada

--- app/views/cliente/home-cliente.php
+++ app/views/cliente/home-cliente.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-
-    <link rel="stylesheet" href="<?= URL . 'public/css/bem-vindo.css' ?>">
-    <link rel="stylesheet" href="<?= URL . 'public/css/home-cliente.css' ?>">
-    <link rel="stylesheet" href="<?= URL . 'public/css/bootstrap.min.css' ?>">
-    <link rel="stylesheet" href="<?= URL . 'public/fontawesome/css/all.min.css' ?>">
-    <link rel="stylesheet" href="<?= URL . 'public/fontawesome/css/fontawesome.min.css' ?>">
-    <title>Bem-vindo</title>
-</head>
-
-<body>
-    <div class="conteudo">
-        <?php require_once 'navbar-cliente.php'; ?>
+        <?php require_once 'sidebar-cliente.php'; ?>
 
         <div class="corpo">
             <div class="conteudo-corpo">
@@ -67,8 +44,8 @@
                         <span>FAÇA A SUA ESCOLHA:</span>
                         <select id="tipo-escolha" class="form-select" required>
                             <option value="" selected disabled>...</option>
-                            <option>Táxi mais próximo</option>
-                            <option>Selecionar táxi específico</option>
+                            <option value="1">Táxi mais próximo</option>
+                            <option value="2">Selecionar táxi específico</option>
                         </select>
                     </div>
                     <div class="tempo-estimado">
---
+++ app/views/cliente/sidebar-cliente.php
@@ -0,0 +1,36 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= URL . 'public/css/bem-vindo.css' ?>">
+    <link rel="stylesheet" href="<?= URL . 'public/css/home-cliente.css' ?>">
+    <link rel="stylesheet" href="<?= URL . 'public/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="<?= URL . 'public/fontawesome/css/all.min.css' ?>">
+    <link rel="stylesheet" href="<?= URL . 'public/fontawesome/css/fontawesome.min.css' ?>">
+    <title>Tela do Cliente | Táxis UTEC</title>
+</head>
+
+<body>
+    <div class="conteudo">
+        <nav class="sidebar">
+            <div class="logo-sidebar">
+                <a href=""><img src="<?= URL . 'public/img/logo/LOGO-UTEC-BRANCO-PNG.png' ?>" alt=""></a>
+            </div>
+            <hr>
+            <div class="topo-sidebar">
+                <a href="">MÉTODO DE PAGAMENTO</a>
+                <a href="">MEU ENDEREÇO</a>
+                <a href="">CLASSIFICAÇÃO</a>
+                <a href="">HISTÓRICO DE VIAGEM</a>
+            </div>
+            <div class="fundo-sidebar">
+                <i class="fa-solid fa-user"></i><span>Garcia Babe</span>
+            </div>
+        </nav>
